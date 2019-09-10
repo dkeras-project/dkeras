242e2da5f090ced3286a633bd1e973c9cba95606
finch <offercstephen@gmail.com>
2019-09-10 13:06:21 -0700

    utilization test updated

--- testing/utilization_test.py
+++ testing/utilization_test.py
@@ -15,8 +15,8 @@ from tensorflow.keras.applications import ResNet50
 
 
 def run_predict():
+    test_data = np.random.uniform(-1, 1, (1000, 224, 224, 3))
     model = ResNet50()
-    test_data = np.random.uniform(-1, 1, (100, 224, 224, 3))
     time.sleep(1)
     model.predict(test_data)
 
@@ -25,7 +25,7 @@ def monitor(target):
     worker_process = mp.Process(target=target)
     worker_process.start()
     #p = psutil.Process(worker_process.pid)
-
+    time.sleep(1)
     # log cpu usage of `worker_process` every 10 ms
     cpu_percents = []
     while worker_process.is_alive():
@@ -38,21 +38,22 @@ def monitor(target):
 
 def main():
     cpu_percents = monitor(target=run_predict)
-    print(cpu_percents)
 
     # Data for plotting
     t = np.asarray(range(len(cpu_percents)))
     cpu_percents = np.asarray(cpu_percents)
 
-    fig, ax = plt.subplots()
-    ax.plot(t, cpu_percents)
+    for i in cpu_percents:
+        print(i)
 
-    ax.set(xlabel='time (ms)', ylabel='CPU utilization',
-           title='CPU utilization for ResNet50 Inference')
-    ax.grid()
-
-    fig.savefig("test.png")
-    plt.show()
+    # fig, ax = plt.subplots()
+    # ax.plot(t, cpu_percents)
+    #
+    # ax.set(xlabel='time (ms)', ylabel='CPU utilization',
+    #        title='CPU utilization for ResNet50 Inference')
+    # ax.grid()
+    #
+    # fig.savefig("test.png")
 
 
 if __name__ == "__main__":
