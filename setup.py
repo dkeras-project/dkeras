b8c3d732c463b027fbd00cda41a6aebb406e9851
finch <offercstephen@gmail.com>
2019-09-28 22:44:37 -0700

    setup bug

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
-setup(name='dKeras',
+setup(name='dkeras',
       version='0.01',
       description='Distributed Keras Engine',
       author='Stephen Offer',
       author_email='offerstephen@gmail.com',
-      url='https://github.com/gndctrl2mjrtm/dkeras/',
-      packages=['dkeras'],
+      url='https://github.com/dkeras-project/dkeras/',
+      packages=find_packages(),
      )
