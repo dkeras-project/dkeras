21b58bd4f05e0889a5f399849cb93d8b6f24466f
finch <offercstephen@gmail.com>
2019-09-28 22:18:29 -0700

    path bug fix

--- dkeras/__init__.py
+++ dkeras/__init__.py
@@ -4,4 +4,10 @@
 
 """
 from __future__ import print_function, division
+import sys
+import os
+
+dkeras_dirpath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(dkeras_dirpath)
+
 from dkeras.dkeras import dKeras
